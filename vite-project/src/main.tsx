61c0a81d1ded365c2c0027eef869ce6e0e3567a7
2052202 <s2052202@online.houstonisd.org>
2026-05-28 05:36:33 +0000

    Adding React, Sans Fight HTML, Still need to fix running

---
+++ vite-project/src/main.tsx
@@ -0,0 +1,24 @@
+import './index.css';
+import { createRoot } from "react-dom/client";
+
+function Game() {
+  // Points to your public directory folder
+const localGameUrl = "/Game/index.html";
+
+return (
+  <iframe
+    src={localGameUrl}
+    title="Sans Fight Simulator"
+    allow="autoplay; fullscreen; keyboard; gamepad; pointer-lock"
+    allowFullScreen
+    style={{ width: '100%', height: '100%', border: 'none' }}
+  />
+);
+
+}
+
+createRoot(document.getElementById("root")!).render(
+  <>
+
+  </>
+)
