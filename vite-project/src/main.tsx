1f3a058cfe13ff7da10b896ea150380d88db04bd
2052202 <s2052202@online.houstonisd.org>
2026-05-28 07:01:04 +0000

    Bulilt

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -3,22 +3,22 @@ import { createRoot } from "react-dom/client";
 
 function Game() {
   // Points to your public directory folder
-const localGameUrl = "/Jc87.github.io-master/index.html";
+  const localGameUrl = "/Jc87.github.io-master/index.html";
 
-return (
-  <iframe
+  return (
+    <iframe
     src={localGameUrl}
     title="Sans Fight Simulator"
     allow="autoplay; fullscreen; keyboard; gamepad; pointer-lock"
     allowFullScreen
     style={{ width: '100%', height: '100%', border: 'none' }}
-  />
+    />
 );
 
 }
 
 createRoot(document.getElementById("root")!).render(
   <>
-
+  <Game /> 
   </>
 )
